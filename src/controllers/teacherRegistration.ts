b584167212e65b6440fb4d728f5640dfecb69153
A7med-Mido <ahmedashrafodi.4@gmail.com>
2025-09-08 03:04:02 +0300

    edit similar controller and some comment modification

--- src/controllers/teacherRegistration.ts
+++ src/controllers/teacherRegistration.ts
@@ -5,12 +5,17 @@ import { encrypt, hashPassword } from "../utils/encryption";
 
 const teacherRegistration = async (userData: TeacherInput) => {
   try {
+    // validate Teacher data
     const teacherData = teacherSchema.parse(userData)
+    // hashing password
     teacherData.password = hashPassword(userData.password)
+    // encrypting Teacher data
     const { password, phone, user } = teacherData
     const token = encrypt({ phone, password, user })
+    // delete userRole before saving to database
     delete teacherData.user
     await TeacherModel.create(teacherData)
+    // return token to store in the client
     return token
   } catch (error) {
     console.log(error)
