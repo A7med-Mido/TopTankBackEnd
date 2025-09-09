d78e44f3229af14fd579ed0647033320bc2a45b8
A7med-Mido <ahmedashrafodi.4@gmail.com>
2025-09-09 13:17:07 +0300

    image OPT

---
+++ src/routes/studentRoute.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from "express";
+
+
+const studentRoute: Router = Router();
+
+
+
+
+
+
+export default studentRoute
---
+++ src/utils/optImage.ts
@@ -0,0 +1,21 @@
+import sharp from "sharp";
+
+export const optImage = async (imageFile: File) => {
+  const buffer = Buffer.from(await imageFile.arrayBuffer());
+  return await sharp(buffer)
+    .resize({
+      width: 400,
+      height: 400,
+      fit: "cover",
+      position: "center",
+      withoutEnlargement: true,
+    })
+    .webp({
+      quality: 65,       
+      effort: 6,          
+      smartSubsample: true,
+      nearLossless: true,
+    })
+    .withMetadata({ orientation: undefined })
+    .toBuffer();
+};
