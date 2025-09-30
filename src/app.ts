f7cb0d1ad346253b3f69caba69f1f90c6b340e54
A7med-Mido <ahmedashrafodi.4@gmail.com>
2025-09-30 22:55:22 +0300

    i18n progress

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from "express"
 import authRoute from "./routes/auth.route";
 import path from "path";
 import studentRoute from "./routes/user.route";
-
+import i18next from "i18next";
+import middleware from "i18next-http-middleware"
 
 const app = express();
 
 app.use(express.json());
+app.use(middleware.handle(i18next));
 app.use("/public", express.static(path.join(process.cwd(), "public")));
-
 
 
 app.use("/api", studentRoute)
